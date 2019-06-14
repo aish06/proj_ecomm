9d54d741d217a3c7a2f7c1dc19a442d492f2b9ca
Aishwary Srivastava <aishwary.06@gmail.com>
2019-06-14 18:49:46 +0530

    commit

--- proj_ecom/customer_profile/models.py
+++ proj_ecom/customer_profile/models.py
@@ -3,11 +3,23 @@ from django.contrib.auth import authenticate,login,get_user_model,logout
 
 User=get_user_model()
 
+
+
+class ProductManager(models.Manager):
+    def get_by_id(self,id):
+        qs=self.get_queryset().filter(id=id)
+        if qs.count()==1:
+            return qs.first()
+        return None
+
 class Customer(models.Model):
-    name=models.CharField(max_length=100)
+    firstname=models.CharField(max_length=100)
+    lastname=models.CharField(max_length=100)
     username=models.CharField(max_length=100)
     email=models.EmailField(max_length=100)
     phone=models.IntegerField()
 
     def __str__(self):
-        return self.name
+        return self.username
+
+
