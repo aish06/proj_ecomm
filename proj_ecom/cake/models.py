52b06c787313d85901b2850586dad325555e4739
Aishwary Srivastava <aishwary.06@gmail.com>
2019-06-19 20:47:04 +0530

    commit

--- proj_ecom/cake/models.py
+++ proj_ecom/cake/models.py
@@ -11,13 +11,13 @@ def get_filename_ext(filepath):
     return name,ext
 
 
-#def upload_image_path(instance, filename):
-#    # print(instance)
-#    # print(filename)
-#    new_filename = random.randint(1, 868686)
-#    name, ext = get_filename_ext(filename)
-#    final_filename = f'{new_filename}{ext}'
-#    return "cakes/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
+def upload_image_path(instance, filename):
+   # print(instance)
+   # print(filename)
+   new_filename = random.randint(1, 868686)
+   name, ext = get_filename_ext(filename)
+   final_filename = f'{new_filename}{ext}'
+   return "cakes/images/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
 
 
 class ProductManager(models.Manager):
@@ -27,11 +27,13 @@ class ProductManager(models.Manager):
             return qs.first()
         return None
 
+
+
 class Cake(models.Model):
     cake_id = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=0.00)
-    image = models.ImageField(upload_to="cakes/images", null=True, blank=True)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     retailer = models.ForeignKey(Retailer,on_delete=models.CASCADE)
     size = models.DecimalField(decimal_places=1, max_digits=20, default=1.0)
     flavour = models.CharField(max_length=100)
