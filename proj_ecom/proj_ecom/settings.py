20942a58d88c2abc3f9032ed9dd0c555e32a52cf
Aishwary Srivastava <aishwary.06@gmail.com>
2019-06-19 18:56:34 +0530

    commit

--- proj_ecom/proj_ecom/settings.py
+++ proj_ecom/proj_ecom/settings.py
@@ -130,10 +130,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
 ]
 
-STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static")
+STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static","static_root")
 
 MEDIA_URL='/media/'
 
-MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+MEDIA_ROOT=os.path.join(BASE_DIR,"media","media_root")
+
 
 
--- proj_ecom/proj_ecom/urls.py
+++ proj_ecom/proj_ecom/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import index,login_page,homepage,signup_page
 
 from bouquet.views import bouquet_list_view
@@ -36,3 +39,7 @@ urlpatterns = [
     path('watch/', watch_list_view),
     path('card/',card_list_view ),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
